077c5fe6f230fd76276c841bcef5cdd0adb4fb87
Jonathan Ferraro <103221374+jonathanferraro@users.noreply.github.com>
2023-07-07 19:21:18 -0700

    added functionality to search bar

--- view/src/components/SearchBar/SearchBar.js
+++ view/src/components/SearchBar/SearchBar.js
@@ -3,18 +3,37 @@ import './SearchBar.css';
 
 import { useSelector } from 'react-redux';
 import { selectProducts } from '../../store/products/Products.reducers';
+import { useNavigate } from 'react-router-dom';
 
 
 export function SearchBar() {
     const [test, setTest] = useState(false);
     const dropdownRef = useRef(null);
     const products = useSelector(selectProducts);
-    
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchProducts, setSearchProducts] = useState([]);
+    const navigate = useNavigate()
 
 
+    // loopp through poroducts
+    // check product.name and product.description
 
+    const filterProducts = () => {
+        Object.values(products).forEach((product) => {
+            const name = product.name.toLowerCase();
+            // const desc = product.description;
+            if (name.includes(searchTerm.toLowerCase())) {
+                setSearchProducts((prevSearchProducts) => [...prevSearchProducts, product]);
+            }
+        })
+    };
 
+    useEffect(() => {
+        setSearchProducts([]);
 
+        filterProducts();
+        console.log(searchProducts)
+    }, [searchTerm]);
 
   
     const toggleDropdown = () => {
@@ -39,27 +58,30 @@ export function SearchBar() {
     return (
       <div className="profile-dropdown" data-cart-dropdown ref={dropdownRef}>
         <button className="link" data-dropdown-button onClick={toggleDropdown}>
-          <p>SearchBarHere</p>
+        <input
+              type="email"
+              placeholder="Search For Products"
+              value={searchTerm}
+              onChange={(e)=> setSearchTerm(e.target.value)}
+            />
         </button>
   
-        <div
+        { (searchProducts && searchTerm) && <div
           className={`profile-dropdown-menu ${test ? 'active' : ''}`}
           onClick={(e) => e.stopPropagation()}
         >
-          
-          <div className="profile-dropdown-link">
-            <p onClick={() => (window.location.href = '/profile')}>
-              Profile
-            </p>
-          </div>
 
-          <div className="profile-dropdown-link">
-            <p>
-              Logout
-            </p>
-          </div>
 
-        </div>
+        {<div className='search-bar-items'>
+          {searchProducts.map((product) => (
+            <div className='search-bar-product-card' onClick={() => window.location.href = `/products/${product.id}`}>
+                <img src={product.image_url}/>
+                <p>{product.name}</p>
+            </div>
+          ))}
+        </div>}
+
+        </div>}
       </div>
     );
 };
